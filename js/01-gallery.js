b61d44bfb69cde38e911f01fc91f927eeccebd62
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-08-30 20:39:34 +0200

    без esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallertList = document.querySelector('.gallery');
+console.log(gallertList)
+
+// розмітка //
+const markup = galleryItems.map(({preview, original, description}) =>
+{ return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}    
+)
+
+gallertList.insertAdjacentHTML('beforeend', markup.join(''))
+// розмітка //
+
+
+gallertList.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault()
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+    return 
+}
